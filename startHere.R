6824b16aa3bbe6811c5c48d20ffa439415b4f014
TylerMcLaughlin <rtylermclaughlin@gmail.com>
2017-06-01 00:31:03 -0500

    major update to Shiny and markdown documents

---
+++ startHere.R
@@ -0,0 +1,6 @@
+# start writing your R code here!  Before we begin, 
+# run these three commands in the terminal console below excluding the '#' symbol:
+# install.packages(“data.table”)
+# install.packages(“ggplot2”)
+# install.packages(“Rbokeh”)
+
